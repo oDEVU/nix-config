1570a46cba1cf2b54988a1eb2cebcc15632a5fee
DEVU <marcineryk@outlook.com>
2026-07-25 02:16:15 +0200

    Missed monitor setup

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -14,11 +14,6 @@
     ../../modules/apps/dev/c-cpp.nix
   ];
 
-  my.hyprland.monitors = ''
-    hl.monitor({ output = "HDMI-A-1", mode = "1920x1080@75", position = "1080x450", scale = 1 })
-    hl.monitor({ output = "DP-3", mode = "1920x1080@60", position = "0x0", scale = 1, transform = 1 })
-  '';
-
   networking.hostName = "nixoslaptop";
   time.timeZone = vars.timezone;
   console.keyMap = "${vars.keyboardLayout}2";
--- hosts/pc/default.nix
+++ hosts/pc/default.nix
@@ -15,6 +15,11 @@
     ../../modules/apps/dev/android.nix
   ];
 
+    my.hyprland.monitors = ''
+      hl.monitor({ output = "HDMI-A-1", mode = "1920x1080@75", position = "1080x450", scale = 1 })
+      hl.monitor({ output = "DP-3", mode = "1920x1080@60", position = "0x0", scale = 1, transform = 1 })
+    '';
+
   networking.hostName = "nixospc";
   time.timeZone = vars.timezone;
   console.keyMap = "${vars.keyboardLayout}2";
